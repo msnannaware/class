5a864cd95bf7b252bb6e728428791d6ffd91cfa7
msnannaware <maddy.iitm@gmail.com>
2017-05-24 19:18:16 -0700

    Update GaussSeidel1.m

--- matprog/GaussSeidel1.m
+++ matprog/GaussSeidel1.m
@@ -1,12 +1,8 @@
 function y=GaussSeidel(A,b,x,NumIters)
 % Runs the Gauss-Seidel method for solving Ax=b, starting with x and
-% running a maximum of NumIters iterations.
-%
-% The matrix A should be diagonally dominant, and in particular, it should
-% not have any diagonal elements that are zero (a division by zero error
-% will be produced).
-%
-% The output y will be the whole sequence of outputs instead of the final
+
+
+% The output y will be the whole sequence of outputs 
 % value (if x is in R^n, then y will be n x NumIters
 
 D=diag(A);
